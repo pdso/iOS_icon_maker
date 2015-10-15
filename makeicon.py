99154c211e6959593c10b4e334c0cfa37bbf3f3e
starer <star@starer.me>
2015-10-15 16:54:17 +0800

    v1.0

--- makeicon.py
+++ makeicon.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import argparse
+import json
 from PIL import Image
 
 
@@ -14,20 +15,11 @@ ipadic = {'29': ['1', '2'],
           '76': ['1', '2']
           }
 
-
-def makeicon(path, width, scale):
-    img = Image.open(path, 'r')
-    size = int(width) * int(scale), int(width) * int(scale)
-    img.thumbnail(size, Image.ANTIALIAS)
-    icondir = os.path.dirname(path) + '/AppIcon.appiconset'
-    if not os.path.exists(icondir):
-        os.mkdir(icondir)
-    filename = ''
-    if scale == '1':
-        filename = '/Appicon_%s.png' % width
-    else:
-        filename = '/Appicon_%s@%sx.png' % (width, scale)
-    img.save(icondir+filename, 'PNG')
+universaldic = {'29': ['1', '2', '3'],
+                '40': ['1', '2', '3'],
+                '60': ['2', '3'],
+                '76': ['1', '2']
+                }
 
 
 def get_options():
@@ -42,8 +34,89 @@ def get_options():
     return args
 
 
+def makeicon(path, width, scale):
+    img = Image.open(path, 'r')
+    size = int(width) * int(scale), int(width) * int(scale)
+    img.thumbnail(size, Image.ANTIALIAS)
+    icondir = os.path.dirname(path) + '/AppIcon.appiconset'
+    if not os.path.exists(icondir):
+        os.mkdir(icondir)
+    filename = ''
+    if scale == '1':
+        filename = '/Appicon%sx%s.png' % width
+    else:
+        filename = '/Appicon%sx%s@%sx.png' % (width, width, scale)
+    img.save(icondir+filename, 'PNG')
+
+
+def makeicons(path, model):
+    dic = {}
+    if model == 'iPhone':
+        dic = iphonedic
+    elif model == 'iPad':
+        dic = ipadic
+    else:
+        dic = universaldic
+
+    for width, scales in dic.items():
+        for scale in scales:
+            makeicon(path, width, scale)
+    return
+
+
+class ImageModel(object):
+    def __init__(self, size, idiom, filename, scale):
+        self.size = size
+        self.idiom = idiom
+        self.filename = filename
+        self.scale = scale
+
+
+def makecontentjsonitem(model, width, scale):
+    filename = ''
+    if scale == '1':
+        filename = 'Appicon%sx%s.png' % width
+    else:
+        filename = 'Appicon%sx%s@%sx.png' % (width, width, scale)
+
+    return ImageModel("%sx%s" % (width, width),
+                      model,
+                      filename,
+                      "%s" % scale)
+
+
+def makeimagemodelist(paramsdic, model):
+    images = []
+    for width, scales in paramsdic.items():
+        for scale in scales:
+            m = makecontentjsonitem(model, width, scale)
+            images.append(m.__dict__)
+    return images
+
+
+def makecontentjson(path, model):
+    icondir = os.path.dirname(path) + '/AppIcon.appiconset'
+    if not os.path.exists(icondir):
+        os.mkdir(icondir)
+    dic = []
+    if model == 'iPhone':
+        dic = iphonedic
+    elif model == 'iPad':
+        dic = ipadic
+    else:
+        dic = universaldic
+    contentjson = {}
+    contentjson['images'] = makeimagemodelist(dic, model)
+    contentjson['info'] = {'version': 1,
+                           'author': 'xcode'}
+    jsons = json.dumps(contentjson)
+    with open(icondir + '/Contents.json', 'w') as f:
+        f.write(jsons)
+
+
 if __name__ == "__main__":
     args = get_options()
     model = args.m
     filepath = os.path.abspath(args.b.name)
-    makeicon(filepath, '29', '2')
+    makeicons(filepath, model)
+    makecontentjson(filepath, model)
